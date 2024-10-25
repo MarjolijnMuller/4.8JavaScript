732d1edea84ba70650813e13a06198bfa8ac7277
MarjolijnMuller <polle.marjolijn@gmail.com>
2024-10-25 16:27:10 +0200

    opdrachten function.js af

--- functions.js
+++ functions.js
@@ -14,20 +14,42 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool() {
+  return "Novi Hogeschool";
+}
 
+const school = getSchool();
+console.log(school);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function getCompliment(name) {
+  return "Lekker bezig met die functies, " + name + "!";
+}
 
+const compliment = getCompliment("Marjolijn");
+console.log(compliment);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+  return [8, 8.5, 6, 7];
+}
 
+const grades = getGrades();
+console.log(grades);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+  return {firstName: "Marjolijn", lastName: "Muller"};
+}
+
+const details = getDetails();
+console.log(details);
